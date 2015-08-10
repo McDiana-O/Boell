15a94cf998b91863ca834cd02ee7f92f67ef162b
shiniwes <shiniwes@hotmail.com>
2015-08-09 19:35:15 -0500

    (General): Rebuild segundo para tener la misma versión ambos tres...

---
+++ What The Frack/Assets/Scripts/Juego01/AlmaJuego.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AlmaJuego : MonoBehaviour {
+	public int time;
+	public Text timer;
+	static int totalTree= 49;
+	public int totalArboles;
+	public enum stateGame{Taladora,Aplanadora,Mezcladora,Gano,Perdio};
+	public stateGame  MyStateGame;
+	public GameObject carrito;
+	private Animator animCarrito;
+
+	public GameObject MenuWinLose;
+	// Use this for initialization
+	void Start () {
+		MyStateGame = stateGame.Taladora;
+		StartCoroutine (countdown());
+		totalArboles = totalTree;
+		animCarrito = carrito.GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (MyStateGame.Equals (stateGame.Taladora) && totalArboles<=0) {
+			totalArboles=totalTree;
+			animCarrito.SetBool("estaAplanando",true);
+			MyStateGame= stateGame.Aplanadora;
+		}
+		else if (MyStateGame.Equals (stateGame.Aplanadora) && totalArboles<=0) {
+			totalArboles=totalTree;
+			animCarrito.SetBool("estaCementado",true);
+			MyStateGame= stateGame.Mezcladora;
+		}
+		else if (MyStateGame.Equals (stateGame.Mezcladora) && totalArboles<=0) {;
+			MyStateGame= stateGame.Gano;
+//			timer.text = "you win!!!";
+			MenuWinLose.SetActive(true);
+			MenuWinLose.GetComponent<ScriptMenuWinLose>().SetMenssageWinorLose(ScriptMenuWinLose.tipoMensaje.Gano);
+		}
+		else if(MyStateGame.Equals (stateGame.Perdio)){
+			//timer.text = "you lost!!!";
+			MenuWinLose.SetActive(true);
+			MenuWinLose.GetComponent<ScriptMenuWinLose>().SetMenssageWinorLose(ScriptMenuWinLose.tipoMensaje.Perdio);
+		}
+
+
+	}
+
+
+	IEnumerator countdown()
+	{
+		while (time > 0 && MyStateGame!=stateGame.Gano)
+		{
+			//timer.text = time.ToString();
+			time -= 1;
+			yield return new WaitForSeconds(1);
+		}
+		if (time <= 0) {
+			MyStateGame = stateGame.Perdio;
+		}
+	}
+}
---
+++ What The Frack/Assets/Scripts/Juego10/GamePlayTen.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class GamePlayTen : MonoBehaviour {
+	public int time;
+	public Text timer;
+	public Text texto;
+	public Button button;
+	public Image agua;
+
+	private float tempPulsos;
+	public enum stateGameMini10{Inicio,Pulsando,Gano,Perdio};
+	stateGameMini10 mysate;
+
+	public GameObject MenuWinLose;
+
+	// Use this for initialization
+	void Start () {
+		mysate = stateGameMini10.Inicio;
+		StartCoroutine (countdown());
+		//tempPulsos = (recTransformAgua.localScale.y *100)/2;
+		//texto.text = tempPulsos.ToString();
+	}
+
+	void FixedUpdate(){
+		if (agua.fillAmount == 0 && mysate != stateGameMini10.Perdio) {
+			mysate = stateGameMini10.Gano;
+			button.interactable = false; 
+			//texto.text = "Ganaste";
+			MenuWinLose.SetActive(true);
+			MenuWinLose.GetComponent<ScriptMenuWinLose>().SetMenssageWinorLose(ScriptMenuWinLose.tipoMensaje.Gano);
+		} else if (time <= 0 && mysate != stateGameMini10.Gano) {
+			mysate = stateGameMini10.Perdio;
+			//texto.text = "Perdiste";
+			MenuWinLose.SetActive(true);
+			MenuWinLose.GetComponent<ScriptMenuWinLose>().SetMenssageWinorLose(ScriptMenuWinLose.tipoMensaje.Perdio);
+		}
+	}
+
+	public void CuentaPulsadas(){
+
+		if (agua.fillAmount > 0 && (mysate != stateGameMini10.Perdio || mysate != stateGameMini10.Gano)) {
+			mysate = stateGameMini10.Pulsando;
+			//recTransformAgua.localScale = new Vector3(1,recTransformAgua.localScale.y-0.05f,1);
+			//tempPulsos = (recTransformAgua.localScale.y *100)/2;
+			agua.fillAmount -= 0.1f;
+			texto.text = tempPulsos.ToString ();
+		} 
+	}
+
+
+	IEnumerator countdown()
+	{
+		while (time >=0 && mysate!= stateGameMini10.Gano)
+		{
+			time -= 1;
+			timer.text = time.ToString();
+			yield return new WaitForSeconds(1);
+		}
+	}
+}
