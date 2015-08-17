55a24da3588a02e8f6f7557e4505ffa50daec63e
johnsvargas <johnsvargas@users.noreply.github.com>
2015-08-16 22:47:08 -0500

    Se agrego el menu de tarjetas  y un build Prototipo4.apk

---
+++ What The Frack/Assets/Scripts/menuTarjetas.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class menuTarjetas : MonoBehaviour {
+	
+	public GameObject tarjetaCanvas;
+	public Text txt_contenido,txt_titulo,txt_numTerjeta;
+	public string[] contenidos;
+	public string[] titulos;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public void activaTarjeta(int numeroTarjeta){
+		tarjetasCanvasHide (true);
+		txt_numTerjeta.text = "0" + numeroTarjeta;
+		txt_contenido.text= contenidos[numeroTarjeta-1];
+		if (numeroTarjeta < 16) {
+			txt_titulo.text=titulos[numeroTarjeta-1];
+		}
+	}
+	public void tarjetasCanvasHide(bool isHide){
+		tarjetaCanvas.SetActive (isHide);
+
+	}
+
+	public void goToMenu(){
+		Application.LoadLevel ("MenuPrincipal");
+	}
+}
