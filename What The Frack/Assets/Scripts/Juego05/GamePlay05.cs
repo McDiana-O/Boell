1037b33d64f85e94ff3c447d730b0f1b469556b8
Diana Ortiz <dkortiz.90@gmail.com>
2015-08-20 14:20:12 -0500

    Se agregaron los  minijuegos 05 ,09, 14

---
+++ What The Frack/Assets/Scripts/Juego05/GamePlay05.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GamePlay05 : MonoBehaviour {
+
+	public Vector2 coordenadas;
+	public GameObject Bar;
+	RectTransform rt;
+
+	// Use this for initialization
+	void Start () {
+		rt = Bar.GetComponent (typeof (RectTransform)) as RectTransform;
+		//rt.sizeDelta = new Vector2 (320, 5500);
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		coordenadas= Camera.main.ScreenToWorldPoint( Input.mousePosition);
+		Debug.LogError (coordenadas.y);
+		rt.offsetMin = new Vector2 (rt.offsetMin.x,608.0f+(coordenadas.y+3.6f)*800.0f);
+	}
+
+	//void
+	//{
+		//MenuWinLose.SetActive(true);
+			//MenuWinLose.GetComponent<ScriptMenuWinLose>().SetMenssageWinorLose(ScriptMenuWinLose.tipoMensaje.Perdio);
+	//}
+}
---
+++ What The Frack/Assets/Scripts/Juego14/GamePlay14.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GamePlay14 : MonoBehaviour {
+
+	private int gender;
+	public float time;
+	public Sprite Dude;
+	public Sprite Gurl;
+	public GameObject Person;
+	public GameObject GirlStuff;
+	public GameObject GirlStuff2;
+	public GameObject MenuWinLose;
+	//private GameObject [] Organs;
+	bool win;
+	//este minijuego dura 8 seg en los 3 niveles
+
+
+	// Use this for initialization
+	void Start () {
+		gender = Random.Range (0, 2);
+		win = false;
+		//Organs[] = 
+		Debug.Log (gender);
+		if (gender == 0) {
+			Person.GetComponent<Image> ().sprite = Dude;
+			GirlStuff.SetActive(false);
+			GirlStuff2.SetActive(false);
+		}
+		if (gender == 1) {
+			Person.GetComponent<Image> ().sprite = Gurl;
+		}
+		StartCoroutine (SetElements ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	//gameObject.SetActive (true);
+
+	IEnumerator SetElements(){
+		while (time > 0 && !win) {
+			time-=0.2f;
+
+			if(time > 1){
+
+				/*if (gender == 0) {
+					Instantiate(Dude,new Vector3(this.transform().x,this.transform().y,this.transform().z), Quaternion.identity);
+				}
+				if (gender == 1) {
+					Instantiate(Gurl,new Vector3(this.transform().x,this.transform().y,this.transform().z), Quaternion.identity);
+				}*/
+
+
+				}
+				yield return new WaitForSeconds(0.2f);
+			}
+		if (time <= 0){
+			MenuWinLose.SetActive(true);
+			MenuWinLose.GetComponent<ScriptMenuWinLose>().SetMenssageWinorLose(ScriptMenuWinLose.tipoMensaje.Perdio);
+		}
+	}
+	
+}
