c9e90f3e22b368af2e73f0e3ca40598b13727c33
johnsvargas <johnsvargas@users.noreply.github.com>
2015-09-20 14:37:23 -0500

    Minujego09 Listo

--- What The Frack/Assets/Scripts/Juego09/GamePlay09.cs
+++ What The Frack/Assets/Scripts/Juego09/GamePlay09.cs
@@ -2,44 +2,143 @@
 using System.Collections;
 
 public class GamePlay09 : MonoBehaviour {
+	//Objetos para tutorial y Tarjeta informativa
+	public GameObject TarjestasInformativas; 
+	public GameObject CanvasTutorial;
+	public GameObject MenuWinLose;
+
 	public GameObject _grieta;
-	public Vector2[] iniPosition;
-	public float time;
-	int numRandom=0;
-	public GameObject _tempGrieta;
-	public int totalExplosivos;
+	private GameObject _tempGrieta;
+
+	private GameObject _textureTube;
+	private float timeToGrietas;
+	private int numRandom=0;
+	private Vector2[] iniPosition;
+	public float[] timeGame;
+	private int[] totalExplosivos;
+	private float[] timeWaterAnim;
+	private WaterAnimacion _waterAnimation;
+
+	enum stateGame{Begin,InGame,Win,Lose,Pause};
+	stateGame myState;
 	public int nivel;
+
+	//For the touch
+	private Touch myTouch;
+	private int tapCount;
+	//para  cambiar  la  la grieta.
+	private GameObject _tempTouchGrieta;
+	private MoveExplosivo _moveExplosivo; 
+
+	//esperando un segundo
+	private timedown _timeDown;
+	private float time=1;
+	//Audios
+	public AudioSource _audioTema;
+	public AudioClip[] winloseAudio;
 	// Use this for initialization
 	void Start () {
-		//everySeconds = 0.5f;
-		time = 4.5f;
-		totalExplosivos = 14;
-		StartCoroutine (CreaGrietas());
+		_timeDown = GameObject.FindGameObjectWithTag ("Clock").GetComponent<timedown> ();
+		//nivel=PlayerPrefs.GetInt ("Nivel");
+
+		timeGame = new float[]{0.0f,0.75f,0.45f,0.3f};
+		totalExplosivos = new int[]{0,6,10,14};
+		iniPosition = new Vector2[]{new Vector2 (10.0f,-7.24f),new Vector2 (10.0f,7.24f)};
+		timeWaterAnim = new float[]{0,0.175f,0.35f,0.525f};
+		_textureTube = GameObject.Find ("TextureTube");
+		_waterAnimation = _textureTube.GetComponent<WaterAnimacion> ();
+		_waterAnimation.scrollSpeed = timeWaterAnim [0];
+		timeToGrietas = 4.5f;
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (myState == stateGame.InGame && totalExplosivos [nivel] > 0  && !_timeDown.isTimeOver) {
+			touchToBoomb ();
+		} 
+		else if (myState != stateGame.Win && totalExplosivos [nivel] == 0 && !_timeDown.isTimeOver) {
+			myState= stateGame.Win;
+			_timeDown.ActivateClock=false;
+			_audioTema.Stop();
+			_audioTema.PlayOneShot(winloseAudio[0],0.6f);
+			StartCoroutine (countdown());
+		}
+		else if (myState == stateGame.InGame && totalExplosivos [nivel] > 0 && _timeDown.isTimeOver) {
+			myState = stateGame.Lose;
+			_timeDown.ActivateClock=false;
+			_audioTema.Stop();
+			_audioTema.PlayOneShot(winloseAudio[1],0.6f);
+			StartCoroutine (countdown());
+		}
+		if ((myState == stateGame.Lose || myState == stateGame.Win) && time<=0) {
+			MenuWinLose.SetActive(true);
+			MenuWinLose.GetComponent<ScriptMenuWinLose>().SetMenssageWinorLose(myState == stateGame.Win ? ScriptMenuWinLose.tipoMensaje.Gano : ScriptMenuWinLose.tipoMensaje.Perdio);
+		}
+
 	}
 
 	IEnumerator CreaGrietas()
 	{
-		//timer.text = time.ToString();
-		while (time>0.0f/* && totalExplosivos!=0*/){
-			yield return new WaitForSeconds(0.3f);
+		while (timeToGrietas>0.0 && myState== stateGame.InGame){
+			yield return new WaitForSeconds(timeGame[nivel]);
 			numRandom = Random.Range (1, 101);
 			numRandom = numRandom >49?1:0;
 
 			_tempGrieta=(GameObject)Instantiate(_grieta,iniPosition[numRandom],Quaternion.identity);
 			_tempGrieta.transform.localScale = numRandom==1?new Vector3(0.7577969f,0.7577969f,0.7577969f):new Vector3(0.7577969f,-0.7577969f,0.7577969f);
 			_tempGrieta.GetComponent<MoveExplosivo> ().RunExplosivo(nivel);
-			time-=0.3f;
+			timeToGrietas-=timeGame[nivel];
 			//totalExplosivos--;
 		}
+	}
 
-			
+	void touchToBoomb(){
+		tapCount = Input.touchCount;
+		for (int i = 0 ; i < tapCount ; i++ ) {
+			myTouch = Input.GetTouch(i);
 
-			//timer.text = time.ToString();
+			if(tapCount > 0 && ( myTouch.phase == TouchPhase.Began || myTouch.phase == TouchPhase.Moved || myTouch.phase == TouchPhase.Stationary)){
 
+				Vector2 pos =myTouch.position;
+				
+				RaycastHit2D hitInfo=Physics2D.Raycast(Camera.main.ScreenToWorldPoint(pos),Vector2.zero);
+
+				if (hitInfo){
+					_tempTouchGrieta  = hitInfo.transform.gameObject;
+					Debug.Log (_tempTouchGrieta.tag);
+					if(_tempTouchGrieta.tag =="crack" /*&& tempCrackHide.myState ==CrackHide.stateCrack.Grieta && juegosoul6.MyStateGame== AlmaJuego6.stateGame.InGame*/)
+					{
+						_moveExplosivo = _tempTouchGrieta.GetComponent<MoveExplosivo>();
+						if(!_moveExplosivo.touched){
+							_moveExplosivo.TouchToExplode();
+							totalExplosivos[nivel]--;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	IEnumerator countdown()
+	{
+		while (time >0)
+		{
+			yield return new WaitForSeconds(1.5f);
+			time -= 1;
+		}
+	}
+
+	public void HideCanvasTutorial(){
+		CanvasTutorial.SetActive (false);
+		myState = stateGame.InGame;
+		_waterAnimation.scrollSpeed = timeWaterAnim [nivel];
+		StartCoroutine (CreaGrietas());
+		_timeDown.ActivateClock = true;
+	}
+	
+	public void HideTarjetaInformativa(){
+		TarjestasInformativas.SetActive (false);
+		
 	}
 }
--- What The Frack/Assets/Scripts/Juego09/MoveExplosivo.cs
+++ What The Frack/Assets/Scripts/Juego09/MoveExplosivo.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class MoveExplosivo : MonoBehaviour {
 	public int[] velocidad;
-	public int i;
+	private int i;
 	public GameObject _explosivo;
 	public GameObject _explosion;
 	private Animator _animaGrieta;
@@ -11,7 +11,7 @@ public class MoveExplosivo : MonoBehaviour {
 	public AudioSource _SFX;
 	public AudioClip[] _SoundSFX;
 
-	public bool touchme = false;
+	public bool touched;
 	// Use this for initialization
 	void Start () {
 		_SFX = GameObject.Find ("SFX_Audio").GetComponent<AudioSource>();
@@ -21,28 +21,26 @@ public class MoveExplosivo : MonoBehaviour {
 		_animaGrieta.Play("Grieta"+Random.Range(1,4));
 		_animaGrieta.speed = 0;
 		_animExplosion.speed = 0;
+		touched = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
 		transform.Translate (i==0?Vector3.zero:(Vector3.left/velocidad[i]));
-		if (touchme) {
-			_animaGrieta.speed = 1;
-			_SFX.PlayOneShot(_SoundSFX[0]);
-			_animExplosion.speed = 1;
-			_explosivo.SetActive(false);
-			_SFX.PlayOneShot(_SoundSFX[1]);
-			touchme= false;
-		}
 	}
 
-	public void Explode(){
-		touchme = true;
-
-	}
 	public void RunExplosivo(int index){
 		i = index;
+	}
+
+	public void TouchToExplode(){
+		_animaGrieta.speed = 1;
+		_SFX.PlayOneShot(_SoundSFX[0]);
+		_animExplosion.speed = 1;
+		_explosivo.SetActive(false);
+		_SFX.PlayOneShot(_SoundSFX[1]);
+		touched= true;
 	}
 
 }
