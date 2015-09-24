bcf49c30021bdc211d2e8049e161f26e2b01766b
Diana Ortiz <dkortiz.90@gmail.com>
2015-09-24 06:18:29 -0500

    Menú Principal con logo, trabajando en las tarjetas quiz

---
+++ What The Frack/Assets/Scripts/TarjetaInformativa/PopUpQuiz.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PopUpQuiz : MonoBehaviour {
+
+	public GameObject[] tarjetaDisabled;
+	public GameObject[] tarjetaActive;
+	public GameObject popUpQuiz;
+	public GameObject cerrarPopUp;
+	private bool PlayNow;
+	public int idCard;
+	public string[] contenidos;
+	public Text txt_contenido;
+	//public enum tipoMensaje{Gano,Perdio};
+
+	// Use this for initialization
+	void Start () {
+		if (PlayNow) {
+			InicializaQuiz (idCard);
+		}
+	}
+	// Update is called once per frame
+	//void Update () {
+	//}
+
+	public void popUpActivado (int numeroTarjeta){
+		Destroy(tarjetaDisabled[numeroTarjeta-1]);
+		tarjetaActive [numeroTarjeta - 1].SetActive (true);
+	}
+
+	public void InicializaQuiz(int numeroTarjeta){
+
+		txt_contenido.text= contenidos[numeroTarjeta-1];
+		//txt_titulo.text = System.Text.RegularExpressions.Regex.Unescape(titulos [numeroTarjeta - 1]);
+		//imgCard.sprite = ImgCards [numeroTarjeta - 1]
+
+	}
+
+
+
+
+}
