6b9174e806b6ce0ec0b7d6f350add2d4b8016eb2
Diana Ortiz <dkortiz.90@gmail.com>
2015-09-24 14:47:16 -0500

    Menu de tarjetas. Falta conexión con playerPrefs.

--- What The Frack/Assets/Scripts/TarjetaInformativa/PopUpQuiz.cs
+++ What The Frack/Assets/Scripts/TarjetaInformativa/PopUpQuiz.cs
@@ -6,36 +6,93 @@ public class PopUpQuiz : MonoBehaviour {
 
 	public GameObject[] tarjetaDisabled;
 	public GameObject[] tarjetaActive;
+	public int[] correctAnswer;
+	public int[] cardCost;  //ESTE ES EL ARREGLO CON EL COSTO DE LAS TARJETAS 
 	public GameObject popUpQuiz;
-	public GameObject cerrarPopUp;
-	private bool PlayNow;
-	public int idCard;
+	public GameObject btnTrue;
+	public GameObject btnFalse;
+	public GameObject tarjetaCanvas;
 	public string[] contenidos;
 	public Text txt_contenido;
-	//public enum tipoMensaje{Gano,Perdio};
+	public Text txt_countTarjetas;  //CUENTA EL NUMERO DE TARJETAS QUE TIENE EL USUARIO
+	public AudioSource _soundWin;
+	public AudioSource _soundClick;
+	public Button cards;
+	public bool isInteractable;
+	public int interactableCards;
+	public int idCartaActiva;
 
 	// Use this for initialization
-	void Start () {
-		if (PlayNow) {
-			InicializaQuiz (idCard);
-		}
-	}
-	// Update is called once per frame
-	//void Update () {
-	//}
+	public void minigameCardOn (int gameCardNum) {
 
-	public void popUpActivado (int numeroTarjeta){
-		Destroy(tarjetaDisabled[numeroTarjeta-1]);
-		tarjetaActive [numeroTarjeta - 1].SetActive (true);
+		//cards.IsActive (true);
+		//txt_countTarjetas.text = Text
+	}
+
+
+	public void TarjetaHide (int numeroTarjeta){
+		idCartaActiva = numeroTarjeta;
+		tarjetaDisabled [numeroTarjeta].SetActive (false);
+		popUpQuiz.SetActive (true);
+		InicializaQuiz (numeroTarjeta);
 	}
 
 	public void InicializaQuiz(int numeroTarjeta){
-
-		txt_contenido.text= contenidos[numeroTarjeta-1];
+		txt_contenido.text= contenidos[numeroTarjeta];
+		tarjetaActive[idCartaActiva].SetActive(true);
 		//txt_titulo.text = System.Text.RegularExpressions.Regex.Unescape(titulos [numeroTarjeta - 1]);
 		//imgCard.sprite = ImgCards [numeroTarjeta - 1]
+	}
+	
+
+	public void compareAnswer(int btnAnswer){
+		//string[] temp = btnAnswer.Split (',');
+		//Debug.Log (temp [0].ToString () + " otro valor: " + temp [1].ToString ());
+		//int id = int.Parse (temp [1]);
+
+		if(btnAnswer==correctAnswer[idCartaActiva]){
+			Debug.Log("Correcto");
+		}
+		else{
+			Debug.Log("Loser");
+		}
+		popUpQuiz.SetActive (false);
+		tarjetaCanvas.SetActive (true);
+		tarjetaCanvas.GetComponent<TarjetaInformativa> ().InicializaTarjeta (idCartaActiva + 17);
+		//tarjetaCanvas.GetComponent<TarjetaInformativa>().idCard=idCardaActiva+16;
+		//tarjetaCanvas.GetComponent<TarjetaInformativa>().PlayNow=true;
+
 
 	}
+	/*
+	public void ActivaTarjeta(int numeroTarjeta){
+		switch (numeroTarjeta) {
+		case 0:
+			//destruyeTarjeta(numeroTarjeta+1);
+			InicializaQuiz(numeroTarjeta+1);
+			if (btnTrue == true ){
+				_soundClick.Play ();
+				tarjetaCanvas.SetActive(true);
+				tarjetaCanvas.SendMessage ("InicializaTarjeta",numeroTarjeta+1);
+				tarjetaActive [numeroTarjeta].SetActive (true);
+
+			}else{
+				if (btnFalse == true){
+					_soundWin.Play ();
+					tarjetaCanvas.SetActive(true);
+					tarjetaCanvas.SendMessage ("InicializaTarjeta",numeroTarjeta);
+					tarjetaActive [numeroTarjeta].SetActive (true);
+					this.gameObject.SetActive(false);
+				}
+		    }
+		    break;
+		}
+		//tarjetaActive [numeroTarjeta - 1].SetActive (true);
+	}*/
+
+
+
+
 
 
 
