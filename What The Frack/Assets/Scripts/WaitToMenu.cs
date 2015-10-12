f7e79323888c1b7e95ef1403f1758f8abe51a1f2
johnsvargas <johnsvargas@users.noreply.github.com>
2015-10-12 18:29:51 -0500

    (Creditos) Funcionado Solo falta los links.

--- What The Frack/Assets/Scripts/WaitToMenu.cs
+++ What The Frack/Assets/Scripts/WaitToMenu.cs
@@ -5,4 +5,15 @@ public class WaitToMenu : MonoBehaviour {
 	public void nextoTarjeta(){
 		Application.LoadLevel ("tarjetainformativa");
 	}
+
+	///funciones para Creditos
+
+	public void GoToScene(string value){
+		Application.LoadLevel (value);
+	}
+
+	public void OpenSitiosOPorUrl(string value)
+	{
+		Application.OpenURL (value);
+	}
 }
