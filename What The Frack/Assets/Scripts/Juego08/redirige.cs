07f888a9b98875c7f700366de89f751ba74a3960
johnsvargas <johnsvargas@users.noreply.github.com>
2016-03-14 11:40:50 -0600

    -Cambios Graficos

--- What The Frack/Assets/Scripts/Juego08/redirige.cs
+++ What The Frack/Assets/Scripts/Juego08/redirige.cs
@@ -7,6 +7,7 @@ public class redirige : MonoBehaviour {
 	private GamePlayerPrefs _playerPrefs;
 	// Use this for initialization
 	void Start () {
+		_playerPrefs= GameObject.FindGameObjectWithTag ("GamePlayerPrefs").GetComponent<GamePlayerPrefs>();
 		textCargando.text = _playerPrefs.Lgui.getString("loadingscreen_text");
 		Time.timeScale=1;
 		Application.LoadLevel ("Juego08");
@@ -14,6 +15,6 @@ public class redirige : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
